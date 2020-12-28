553b15e71d4ede83d533cd4cb87ac70cf9ecec0a
bgrooot <bgrooot@gmail.com>
2020-12-28 13:34:46 +0900

    :pencil2 챌린지 13 풀이

---
+++ kotlin/big-nerd-ranch/challenge13.kt
@@ -0,0 +1,16 @@
+class Sword(_name: String) {
+	var name = _name
+		get() = "The Legendary $field"
+		set(value) {
+			field = value.toLowerCase().reversed().capitalize()
+		}
+
+	init {
+		name = _name
+	}
+}
+
+fun main() {
+	val sword = Sword("Execalibur")
+	println(sword.name)
+}
